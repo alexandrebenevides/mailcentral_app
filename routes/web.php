fee75de5535dba929236b9b48bf7c5a93f072ac8
Alexandre Benevides <alexandre.benevides@gruponewway.com.br>
2023-12-22 01:53:11 -0300

    Implementando middleware para a validação da autenticação e configurando rota para o acesso à dashboard

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,17 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+
+use App\Http\Middleware\UserPermission;
+
 use App\Livewire\Pages\SignIn;
 use App\Livewire\Pages\SignUp;
+use App\Livewire\Pages\Dashboard\MailAccounts;
  
 Route::get('/', SignIn::class);
 Route::get('/signin', SignIn::class);
 Route::get('/signup', SignUp::class);
+
+Route::prefix('/dashboard')->middleware([UserPermission::class])->group(function () {
+    Route::get('/accounts', MailAccounts::class);
+});
